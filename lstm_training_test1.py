52f8b1817cf3869b504d8a90ee2b305b8a7b492b
Jo Ye <jdy15@ic.ac.uk>
2023-01-14 12:21:35 +0000

    Initial commit

---
+++ lstm_training_test1.py
@@ -0,0 +1,113 @@
+import numpy as np
+import pickle
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
+
+# Constants/parameters
+window_size = 1000 # Used in pre-processing
+batch_size = 200 # Used for training
+learning_rate = 0.0001
+n_epochs = 5 # Training epochs - set to a low number for now 
+input_dim = 90
+hidden_dim = 200
+layer_dim = 1
+output_dim = 3 # For now - only looking at bed and fall activities
+
+# Read in data
+print("Reading in data and converting to tensors...")
+with open("data_test1.pk1", "rb") as file:
+    data = pickle.load(file)
+x_train = data[0]
+x_test = data[1]
+y_train = data[2]
+y_test = data[3]
+
+# Convert to torch tensors and reshape x into sequential data (3D)
+x_train_tensor = Variable(torch.Tensor(x_train))
+x_test_tensor = Variable(torch.Tensor(x_test))
+y_train_tensor = Variable(torch.Tensor(y_train))
+y_test_tensor = Variable(torch.Tensor(y_test))
+
+x_train_tensor = torch.reshape(x_train_tensor, (x_train_tensor.shape[0], window_size, -1))
+x_test_tensor = torch.reshape(x_test_tensor, (x_test_tensor.shape[0], window_size, -1))
+
+# Instantiate LSTM model and loss function
+print("Creating LSTM model, loss function and optimiser...")
+
+# LSTM model class
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(LSTMModel, self).__init__()
+        # Number of hidden units
+        self.hidden_dim = hidden_dim
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        # batch_first=True means that input tensor will be of shape (batch_dim, seq_dim, feature_dim)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+
+        # Output layer (linear combination of last outputs)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+        # Initialize cell state
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # out.size() --> batch_size, seq_dim, hidden_dim
+        # out[:, -1, :] --> batch_size, hidden_dim --> extract outputs from last layer
+        out = self.fc(out[:, -1, :]) 
+        # out.size() --> batch_size, output_dim
+
+        # Apply softmax activation to output
+        activation = nn.Softmax(dim=1)
+        out = activation(out)
+
+        return out
+
+model = LSTMModel(input_dim, hidden_dim, layer_dim, output_dim)
+loss_function = nn.CrossEntropyLoss()
+optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Create data loader for batch gradient descent
+print("Creating data loader for batches...")
+
+# Dataset builder class
+class Dataset_builder(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.len = x.shape[0]        
+    # Getting the data
+    def __getitem__(self, index):    
+        return self.x[index], self.y[index]    
+    # Getting length of the data
+    def __len__(self):
+        return self.len
+
+train_dataset = Dataset_builder(x_train_tensor, y_train_tensor)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size)
+
+# Train model
+print("Training model...")
+for n_epoch in range(n_epochs):
+    print(f"Starting epoch number {n_epoch+1}")
+    for i, (inputs, labels) in enumerate(train_loader):
+        if i%10 == 0:
+            print(f"{i} batches processed")
+        optimiser.zero_grad()
+        outputs = model(inputs)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimiser.step()
+        
+    with torch.no_grad():
+        predictions = model(x_test_tensor)
+        test_loss = loss_function(predictions, y_test_tensor)
+        print(f"Model loss after {n_epoch+1} epochs = {test_loss}")
+
